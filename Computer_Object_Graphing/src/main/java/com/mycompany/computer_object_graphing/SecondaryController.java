8f547603e4ecb8bd458c42f513b0ab9f80e1bbfa
mstmark <mstmark@tesla.com>
2021-11-12 07:03:07 -0800

    Java Application for GUI

---
+++ Computer_Object_Graphing/src/main/java/com/mycompany/computer_object_graphing/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.mycompany.computer_object_graphing;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
