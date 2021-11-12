8cfbed06f0e94428ba95a1451a6c7bc4dd944bfc
mstmark <mstmark@tesla.com>
2021-11-12 08:09:54 -0800

    tied application to fxml file and resources

--- Computer_Object_Graphing/src/main/java/com/mycompany/computer_object_graphing/App.java
+++ Computer_Object_Graphing/src/main/java/com/mycompany/computer_object_graphing/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("Computer_Object_Gui"), 1041, 804);
         stage.setScene(scene);
         stage.show();
     }
